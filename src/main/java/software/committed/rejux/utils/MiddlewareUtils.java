921eb96423fe3365ca2c0b8a7c0878f30367bd9e
chrisflatley <chris@commited.software>
2016-07-19 16:21:25 +0100

    Reimplement middleware utils as common code

--- src/main/java/software/committed/rejux/utils/MiddlewareUtils.java
+++ src/main/java/software/committed/rejux/utils/MiddlewareUtils.java
@@ -1,10 +1,10 @@
 package software.committed.rejux.utils;
 
 import java.util.List;
+import java.util.function.Supplier;
 
 import software.committed.rejux.interfaces.Dispatcher;
 import software.committed.rejux.interfaces.Middleware;
-import software.committed.rejux.interfaces.State;
 
 public final class MiddlewareUtils {
 
@@ -14,32 +14,26 @@ public final class MiddlewareUtils {
 
   public static <S> Dispatcher createChain(final Dispatcher firstDispatcher, final S store,
       final List<Middleware<? super S>> middlewares, final Dispatcher lastDispatcher) {
+    Supplier<S> supplier = () -> store;
+    return createChain(firstDispatcher, supplier, middlewares, lastDispatcher);
+  }
+
+  public static <S> Dispatcher createChain(final Dispatcher firstDispatcher,
+      final Supplier<S> store, final List<Middleware<? super S>> middlewares,
+      final Dispatcher lastDispatcher) {
     Dispatcher dispatcher = lastDispatcher;
 
     if (middlewares != null && !middlewares.isEmpty()) {
       for (int i = middlewares.size() - 1; i >= 0; i--) {
         final Middleware<? super S> m = middlewares.get(i);
         final Dispatcher previous = dispatcher;
-        dispatcher = (action) -> m.apply(firstDispatcher, store, action, previous);
+        dispatcher = (action) -> m.apply(firstDispatcher, store.get(), action, previous);
       }
     }
 
     return dispatcher;
   }
 
-  public static <S> Dispatcher createChain(final Dispatcher firstDispatcher, final State<S> holder,
-      final List<Middleware<? super S>> middlewares, final Dispatcher lastDispatcher) {
-    Dispatcher dispatcher = lastDispatcher;
 
-    if (middlewares != null && !middlewares.isEmpty()) {
-      for (int i = middlewares.size() - 1; i >= 0; i--) {
-        final Middleware<? super S> m = middlewares.get(i);
-        final Dispatcher previous = dispatcher;
-        dispatcher = (action) -> m.apply(firstDispatcher, holder.get(), action, previous);
-      }
-    }
-
-    return dispatcher;
-  }
 
 }
